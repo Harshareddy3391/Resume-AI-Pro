176cf45a82fe3e6d8bb369a30e3bdb8e38189f46
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-07-18 01:08:17 +0530

    updated env file and config file with google keys

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,10 +2,33 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+
+    # ==========================
+    # Application
+    # ==========================
     APP_NAME: str
     APP_VERSION: str
     DEBUG: bool
+
+
+    # ==========================
+    # Database
+    # ==========================
     DATABASE_URL: str
+
+    # ==========================
+    # Google OAuth
+    # ==========================
+    GOOGLE_CLIENT:str
+    GOOGLE_CLIENT_SECRET:str
+    GOOGLE_REDIRECT_URL:str
+
+       # ==========================
+    # JWT
+    # ==========================
+    JWT_SECRET_KEY:str
+    JWT_ALGORITHM:str
+    ACCESS_TOCKEN_EXPIRE_MINUTES:int
 
     model_config = SettingsConfigDict(
         env_file=".env",
