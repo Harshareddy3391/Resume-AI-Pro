13c8bc92d910bf1dff2e5f2ffef343b63eea104a
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-07-18 15:33:05 +0530

    main fiile updated again

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,37 @@
 from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
 from app.core.config import settings
+from app.api.auth import router
 
 app=FastAPI(
     title=settings.APP_NAME,
-    version=settings.APP_VERSION
+    version=settings.APP_VERSION,
+    debug=settings.DEBUG
 
 )
 
+
+#session middleware (required for google auth)
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=settings.JWT_SECRET_KEY
+)
+
+#include router
+app.include_router(router)
 @app.get("/")
-def root_a():
+def root():
     return {
         "message":"welcome to DocuChat AI",
-        "debug":settings.DEBUG
+        "debug":settings.APP_VERSION
+    }
+
+
+
+
+@app.get("/health")
+def health_check():
+    return {
+        "status":"healthy",
+        "application":settings.APP_NAME
     }
