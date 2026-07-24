b8db7717cc6fad2a1872033e3c26997c9cddb51f
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-07-24 21:54:11 +0530

    testing module cmplt

--- backend/app/api/document.py
+++ backend/app/api/document.py
@@ -5,15 +5,12 @@ This file handles all document-related HTTP endpoints.
 
 Responsibilities:
 1. Upload PDF
-2. Get all Uploaded PDFs
-3. Delete PDF
+2. Delete PDF
 
 The Router does NOT contain business logic.
 
 It simply calls the corresponding service function.
 """
-
-from typing import List
 
 from fastapi import APIRouter, Depends, UploadFile, File, status
 from sqlalchemy.orm import Session
@@ -22,13 +19,11 @@ from app.db.database import get_db
 from app.dependencies.auth import get_current_user
 
 from app.models.user_model import User
-
 from app.schemas.document_schema import DocumentResponse
 
 from app.services.document_service import (
     create_document,
-    delete_document,
-    get_user_documents
+    delete_document
 )
 
 # Create router
@@ -50,40 +45,11 @@ def upload_document(
 ):
     """
     Upload a PDF document.
-
-    Flow:
-    Client
-        ↓
-    API
-        ↓
-    document_service.create_document()
-        ↓
-    Supabase Storage
-        ↓
-    PostgreSQL
     """
 
     return create_document(
         db=db,
         file=file,
-        current_user=current_user
-    )
-
-
-@router.get(
-    "/",
-    response_model=List[DocumentResponse]
-)
-def get_documents(
-    db: Session = Depends(get_db),
-    current_user: User = Depends(get_current_user)
-):
-    """
-    Return all documents uploaded by the logged-in user.
-    """
-
-    return get_user_documents(
-        db=db,
         current_user=current_user
     )
 
